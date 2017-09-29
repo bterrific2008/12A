dc80fdf118d4178f6c6e5f68b25196f0c471ed1a
Mitchell P <mjpon45@gmail.com>
2017-09-29 12:38:30 -0700

    [Commit 3] Resyncing
    - Fixing up some errors in git

--- PayCalc/src/Check/PayCheck.java
+++ PayCalc/src/Check/PayCheck.java
@@ -17,12 +17,11 @@ public class PayCheck {
 		while(valid==true) {
 		System.out.println("Please enter the total hours worked Monday through Sunday.");
 		int u = reader.nextInt();
-		System.out.println("Please enter your pay rate (in terms of USD per hour.");
+		System.out.println("Please enter your pay rate (in terms of USD per hour.)");
 		double n = reader.nextDouble();
 		System.out.println("Your paycheck for this week is:" + addingHours(u,n));
 		}
-	}}
-		
+	}	
 	public static double addingHours(int hours, double payAmount) {
 		double total;
 		Scanner reader =  new Scanner(System.in);
@@ -31,8 +30,7 @@ public class PayCheck {
 			System.out.println("What is your overtime pay?");
 			double ovTimPay = reader.nextDouble();
 			total =  (40 * payAmount) + (overtime * ovTimPay);
-		}else if(hours = ){
-			
-			
+		}else if(hours>=40){
 	}
-	}
+		
+}
