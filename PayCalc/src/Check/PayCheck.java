fbbd20226f6b2cb2662eff24f4a46ac6a285f158
Mitchell P <mjpon45@gmail.com>
2017-09-29 13:46:19 -0700

    [Commit 4] Finished with standard methods
    - Finished, but read the directions wrong for modifying the overtime

--- PayCalc/src/Check/PayCheck.java
+++ PayCalc/src/Check/PayCheck.java
@@ -13,24 +13,30 @@ public class PayCheck {
 	public static void main(String args[]) {
 		Scanner reader = new Scanner(System.in);
 		boolean valid = true;
-		
+
 		while(valid==true) {
 		System.out.println("Please enter the total hours worked Monday through Sunday.");
-		int u = reader.nextInt();
+		String hours = reader.next();
+		double u = Double.parseDouble(hours);
 		System.out.println("Please enter your pay rate (in terms of USD per hour.)");
-		double n = reader.nextDouble();
-		System.out.println("Your paycheck for this week is:" + addingHours(u,n));
+		String pay = reader.next();
+		double n = Double.parseDouble(pay);
+		System.out.println("Your paycheck for this week is: $" + addingHours(u,n));
 		}
 	}	
-	public static double addingHours(int hours, double payAmount) {
-		double total;
+	public static double addingHours(double hours, double payAmount) {
+		double total = 0.0;
 		Scanner reader =  new Scanner(System.in);
 		if(hours>40) {
-			int overtime = hours - 40;
+			double overtime = hours - 40;
 			System.out.println("What is your overtime pay?");
 			double ovTimPay = reader.nextDouble();
 			total =  (40 * payAmount) + (overtime * ovTimPay);
-		}else if(hours>=40){
+		}else if(hours<=40){
+			total =  (40 * payAmount);
+		}
+		return total;
 	}
-		
-}
+	public static String checker(String input) {
+		if(input.)
+	}
