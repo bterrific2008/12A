0b2eb9f004e22e84a01b142fa037abddc5d9e1e5
Mitchell P <mjpon45@gmail.com>
2017-09-29 13:57:39 -0700

    [Done]
    Completed the assignment

--- PayCalc/src/Check/PayCheck.java
+++ PayCalc/src/Check/PayCheck.java
@@ -1,12 +1,16 @@
+//PayCheck.java
+//Mitchell Pon
+//ID: 1591702
+//Programming Assignment 1
+// A Basic calculator that figures out the final wage of the week. You must enter your hours and pay. Overtime pay is added
+// on via 1.5 times the standard pay
+
+
+
 package Check;
 
 import java.util.Scanner;
 
-//file_name.java
-//Mitchell Pon
-//your CruzID
-//Pay
-//a (very) short description of the program
 
 public class PayCheck {
 
@@ -14,29 +18,21 @@ public class PayCheck {
 		Scanner reader = new Scanner(System.in);
 		boolean valid = true;
 
-		while(valid==true) {
 		System.out.println("Please enter the total hours worked Monday through Sunday.");
-		String hours = reader.next();
-		double u = Double.parseDouble(hours);
+		double hours = reader.nextDouble();
 		System.out.println("Please enter your pay rate (in terms of USD per hour.)");
-		String pay = reader.next();
-		double n = Double.parseDouble(pay);
-		System.out.println("Your paycheck for this week is: $" + addingHours(u,n));
-		}
+		double pay = reader.nextDouble();
+		System.out.println("Your paycheck for this week is: $" + addingHours(hours,pay));
 	}	
 	public static double addingHours(double hours, double payAmount) {
 		double total = 0.0;
 		Scanner reader =  new Scanner(System.in);
 		if(hours>40) {
 			double overtime = hours - 40;
-			System.out.println("What is your overtime pay?");
-			double ovTimPay = reader.nextDouble();
-			total =  (40 * payAmount) + (overtime * ovTimPay);
+			total =  (40 * payAmount) + (overtime * payAmount * 1.5);
 		}else if(hours<=40){
 			total =  (40 * payAmount);
 		}
 		return total;
 	}
-	public static String checker(String input) {
-		if(input.)
-	}
+}
