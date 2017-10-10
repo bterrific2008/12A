a72b89a14eb3aa5d3669a7514493dc9e5f50b727
Mitchell P <mjpon45@gmail.com>
2017-10-10 12:39:29 -0700

    [Commit - Proyecto 2] Started the barebones
    - Created a randNum Generator
    - created the intro handlers
    - Created the checker method to handle the checking de los numbres.

--- GuessingGame/src/Game/GuessingGame.java
+++ GuessingGame/src/Game/GuessingGame.java
@@ -13,6 +13,27 @@ public class GuessingGame {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
+		String inputname;
+		int guess1, guess2, guess3;
+		// Random number generator, doubles then goes the int
+		double randNum =  0;
+		randNum = (int) (Math.random() *10);
+		System.out.println(randNum);
+		
+		// The actual game: Asks for name and ask for the guesses. Tells if you failed or not
+		System.out.println("Welcome to the number guessing game. What is your honorable name?");
+		inputname = scnr.nextLine();
+		System.out.println("I’m thinking of an integer between 1 and 10. You have 3 guesses.");
+		System.out.print(" Your first guess: ");
+	}
+	//checks if the number is the right guess
+	public static String checker(int input, String input2, int randNum) { 
+		String winning = ("Congratulations" + input2 + "! " + "You won in 2 guesses.");
+		String loser1 = "Too high, guess lower.";
+		String loser2 = "Too low, guess higher.";
+		if(input>)
+		
+		return winning;
 	}
 
 }
