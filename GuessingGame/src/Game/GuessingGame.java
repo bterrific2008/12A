274b0e9fc41562e90f0603dac1635b76b8278c83
Mitchell P <mjpon45@gmail.com>
2017-10-10 12:08:38 -0700

    [Commit - Proyecto] Guessing Game start
    - Just added the start of the project
     - Just the normal git process of syncing

---
+++ GuessingGame/src/Game/GuessingGame.java
@@ -0,0 +1,18 @@
+package Game;
+
+import java.util.Scanner;
+
+//GuessingGame.java
+//Mitchell Pon
+//CruzID: mjpon
+//Programming Assignment 1
+//A Basic calculator that figures out the final wage of the week. You must enter your hours and pay. Overtime pay is added
+//on via 1.5 times the standard pay
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+	}
+
+}
